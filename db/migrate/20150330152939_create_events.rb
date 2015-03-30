784211aca7de0fabd58cf0b549418324aad15039
agoldvarg <agoldvarg@gmail.com>
2015-03-30 11:31:45 -0400

    create initial migrations and models for event, device, and manufacturer

---
+++ db/migrate/20150330152939_create_events.rb
@@ -0,0 +1,16 @@
+class CreateEvents < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.string :type
+      t.string :reporter
+      t.string :content
+      t.string :product_code
+      t.datetime :event_date
+      t.datetime :receive_date
+      t.integer :device_id
+      t.integer :manufacturer_id
+
+      t.timestamps null: false
+    end
+  end
+end
