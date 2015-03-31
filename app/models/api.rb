d07591dd1be95dc2e946a861aebc26b30f98734c
Skylar Panuska <skylar.panuska@gmail.com>
2015-03-31 10:19:20 -0400

    adds Heroku deployment files

--- app/models/api.rb
+++ app/models/api.rb
@@ -3,7 +3,7 @@ class Api < ActiveRecord::Base
 	require 'open-uri'
 
   def self.call
-    base_url = "https://api.fda.gov/drug/event.json?api_key="
+    base_url = "https://api.fda.gov/device/event.json?api_key=S9BDUNvbSr75OCkTG4ZB8sYzMBPSVjZrIDK0qRdo"
     JSON.load(open(base_url))
   end
 
