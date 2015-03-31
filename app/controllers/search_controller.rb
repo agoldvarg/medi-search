dc6c4a06ee785bbe94ede704863192fbda317cb6
laranicole70 <laranicole70@gmail.com>
2015-03-31 16:49:08 -0400

    test limit increase results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,7 +7,7 @@ class SearchController < ApplicationController
 	def create
     @search_term = params["search"]["search_term"].upcase
     @search_type = search_type
-    @recent_events = Event.where("#{@search_type} like '%#{@search_term}%' AND date_of_event IS NOT NULL").order("date_of_event DESC").limit(5)
+    @recent_events = Event.where("#{@search_type} like '%#{@search_term}%' AND date_of_event IS NOT NULL").order("date_of_event DESC").limit(50)
     render :show
 	end
 
