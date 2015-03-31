45c6c6ccf2b7a406d0e51bb6943ede42ddac8093
Skylar Panuska <skylar.panuska@gmail.com>
2015-03-31 15:41:35 -0400

    adds dropdown menu to search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -7,6 +7,7 @@ class SearchController < ApplicationController
 	def create
     @search_term = params["search"]["search_term"].upcase
     @recent_events = Event.where("generic_name like '%#{@search_term}%' AND date_of_event IS NOT NULL").order("date_of_event DESC").limit(5)
+    binding.pry
     render :show
 	end
 
