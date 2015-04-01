08c651d9aaa1e8d1ae4f91b63717bc74f62a0021
Skylar Panuska <skylar.panuska@gmail.com>
2015-04-01 15:59:04 -0400

    starts to build the more advanced db query string, includes notes for next steps

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,7 +2,19 @@ class Search
 	 attr_accessor :search_term
 	 extend ActiveModel::Naming
    include ActiveModel::Conversion
+   
+   def count
+   	# Event.group(:event_type).count
+
+   	# Event.group(:manufacturer_name).count.sort #=> unsorted array of arrays [[name, count],[name, count]]
+
+
+   	# sorted_manufact_names.select { |element| element.include?("MEDTRONIC") } #=> returns elements that exactly match MEDTRONIC
+   	# NEXT: need to take this logic, covnvert to AREL for db search
+   end
+   
    def persisted?
     false
    end
+
 end
